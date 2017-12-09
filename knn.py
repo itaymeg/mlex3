13116e89aceb119487edacaabc83730389b93644
Itay Megera <itaymegera@gmail.com>
2017-12-09 19:06:09 +0200

    all

---
+++ knn.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import operator
+from sklearn.datasets import fetch_mldata
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import euclidean as l2distance
+
+#fetch mnist data
+mnist = fetch_mldata('MNIST original')
+data = mnist['data']
+labels = mnist['target']
+
+#divide to train and test
+idx = np.random.RandomState(0).choice(70000, 11000)
+train = data[idx[:10000], :].astype(int)
+train_labels = labels[idx[:10000]]
+test = data[idx[10000:], :].astype(int)
+test_labels = labels[idx[10000:]]
+
+
+def our_max(collection, k):
+    """
+    returns (a,b):
+        a - boolean wheter the collection is k sized
+        b - if the collection is k sized, return the index to replace
+    """
+    if len(collection) < k:
+        return (False, 0)
+    else:
+        highest = 0
+        for i in xrange(len(collection)):
+            if(collection[i][0] > collection[highest][0]): # 0 for distance
+                highest = i
+        return (True, highest)
+def knn(image_set, label_set, query, k):
+    distances = []
+    for idx, img in enumerate(image_set):
+        dist = l2distance(query, img)
+        distances.append((idx, dist))
+    sortedDistances = sorted(distances, key=operator.itemgetter(1))
+    nearest_neightbours = sortedDistances[:k]
+    votes = {}
+    for idx, (img_idx, dist) in enumerate(nearest_neightbours):
+        vote = label_set[img_idx]
+        if vote not in votes:
+            votes[vote] = 0
+        votes[vote] += 1
+    sortedVotes = sorted(votes.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+    
+    
+#a
+def worse_knn(image_set, label_set, query, k):
+    best_k = [] # (distance, label)
+    for idx, img in enumerate(image_set):
+        dist = l2distance(query, img)
+        (k_sized, max_idx) = our_max(best_k, k)
+        if not k_sized:
+            best_k.append((dist, label_set[idx]))     
+        else:
+            if dist < best_k[max_idx][0]: # 0 for distance
+                best_k[max_idx] = (dist, label_set[idx])
+    # find for each label its count in the k best distances
+    label_count = {}
+    for idx, nn_data in enumerate(best_k):
+        dist, label = nn_data
+        if label not in label_count:
+            label_count[label] = 0
+        label_count[label] += 1
+    # take the label with the most votes
+    best_label = None
+    for label in label_count:
+        if best_label is None:
+            best_label = (label, label_count[label])
+        else:
+            if best_label[1] < label_count[label]:
+                best_label = (label, label_count[label])
+    return best_label[0]
+    
+#b
+def run_b(k = 10, n = 1000):
+    expirment_data = train[:n]
+    expirment_labels = train_labels[:n]
+    loss_count = 0
+    for idx, data in enumerate(test):
+        if idx % 250 == 0:
+            print "IMG#{0} K#{1} N#{2}".format(str(idx), str(k), str(n))
+        prediction = knn(expirment_data, expirment_labels, data, k)
+        if prediction != test_labels[idx]:
+            loss_count += 1
+    accuracy = 100 * (len(test) - loss_count) / len(test)
+    print "LOSS#{0} TEST#{1} ACC#{2}%".format(str(loss_count), len(test), accuracy)
+    return accuracy
+        
+    
+def run_c(save = False):
+    print '[EXERCISE C] SAVE={0}'.format(save)
+    ks = range(1, 100 + 1)
+    stat = []
+    for k in ks:
+        accuracy = run_b(k=k)
+        stat.append(accuracy)
+    plt.plot(stat, ks, 'y')
+    if save:
+        plt.savefig('figure_C.jpg')
+    plt.show()
+    
+def run_d(save = False):
+    print '[EXERCISE D] SAVE={0}'.format(save)
+    ns = range(100, 5000 + 1, 100)
+    best_k = 1
+    stat = []
+    for n in ns:
+        accuracy = run_b(k=best_k, n=n)
+        stat.append(accuracy)
+    plt.plot(stat, ns, 'y')
+    if save:
+        plt.savefig('figure_D.jpg')
+    plt.show()
+
+def main(actions = None, save = False):
+    if 'b' in actions:
+        run_b()
+    if 'c' in actions:
+        run_c(save)
+    if 'd' in actions:
+        run_d(save)
+if __name__ == '__main__':
+    main(actions= ['c', 'd'], save=True)
---
+++ perceptron.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from sklearn.datasets import fetch_mldata
+import sklearn.preprocessing
+from sklearn.utils import shuffle
+import lib
+import matplotlib.pyplot as plt
+
+mnist = fetch_mldata('MNIST original')
+data = mnist['data']
+labels = mnist['target']
+
+neg, pos = 0,8
+train_idx = np.random.RandomState(0).permutation(np.where((labels[:60000] == neg) | (labels[:60000] == pos))[0])
+test_idx = np.random.RandomState(0).permutation(np.where((labels[60000:] == neg) | (labels[60000:] == pos))[0])
+
+train_data_unscaled = data[train_idx[:6000], :].astype(float)
+train_labels = (labels[train_idx[:6000]] == pos)*2-1
+
+validation_data_unscaled = data[train_idx[6000:], :].astype(float)
+validation_labels = (labels[train_idx[6000:]] == pos)*2-1
+
+test_data_unscaled = data[60000+test_idx, :].astype(float)
+test_labels = (labels[60000+test_idx] == pos)*2-1
+
+# Preprocessing
+train_data = sklearn.preprocessing.scale(train_data_unscaled, axis=0, with_std=False)
+validation_data = sklearn.preprocessing.scale(validation_data_unscaled, axis=0, with_std=False)
+test_data = sklearn.preprocessing.scale(test_data_unscaled, axis=0, with_std=False)
+
+
+train_data = lib.normalize(train_data)
+validation_data = lib.normalize(validation_data)
+test_data = lib.normalize(test_data)
+
+
+def perceptron(train, labels):
+    """
+        train is in shape a,b
+        where a is number of training samples
+        b in number of features
+    """
+    w = np.zeros(train.shape[1])
+    wrong = True
+    while wrong != False:
+        wrong = False
+        for i, x in enumerate(train):
+            prediction = 0
+            if w.dot(x) >= 0:
+                prediction = 1
+            else:
+                prediction = -1
+            label = labels[i]
+            if prediction != label:
+                wrong = True
+                w += label * x
+                
+    return w
+                
+
+   
+
+def run_a():
+    ns = [5, 10, 50, 100, 500, 1000, 5000]
+    acc_table = {}
+    for n in ns:
+        accs = []
+        for i in xrange(100):
+            if i % 20 == 0:
+                print 'N={0}, RUN={1}'.format(n, i)
+            s_x, s_y = shuffle(train_data, train_labels)
+            h = perceptron(s_x[:n], s_y[:n])
+            loss = 0
+            for idx,  sample in enumerate(test_data):
+                prediction = 0
+                if h.dot(sample) >= 0:
+                    prediction = 1
+                else:
+                    prediction = -1
+                label = test_labels[idx]
+                if prediction != label:
+                    loss += 1
+            
+            acc = (float(test_data.shape[0]) - loss) / float(test_data.shape[0])
+            print 'loss ', loss, ' n ', n, ' acc ', acc
+            accs.append(acc)
+        acc_table[n] = np.mean(accs)
+    lib.print_table(acc_table)
+def run_b():
+    h = perceptron(train_data, train_labels)
+    print h
+    #plt.gray()
+    plt.imshow(h.reshape(28, 28), interpolation='nearest')
+    plt.show()
+def run_c():
+    h = perceptron(train_data, train_labels)
+    loss = 0
+    for idx,  sample in enumerate(test_data):
+        prediction = 0
+        if h.dot(sample) >= 0:
+            prediction = 1
+        else:
+            prediction = -1
+        label = test_labels[idx]
+        if prediction != label:
+            loss += 1
+            
+    acc = (float(test_data.shape[0]) - loss) / float(test_data.shape[0])
+    print'accuracy = ', acc
+def run_d():
+    wrong_samples_idx = []
+    h = perceptron(train_data, train_labels)
+    loss = 0
+    for idx,  sample in enumerate(test_data):
+        prediction = 0
+        if h.dot(sample) >= 0:
+            prediction = 1
+        else:
+            prediction = -1
+        label = test_labels[idx]
+        if prediction != label:
+            wrong_samples_idx.append(idx)
+            loss += 1
+    plt.imshow(test_data_unscaled[wrong_samples_idx[1]].reshape(28,28))
+    plt.show()
+        
+if __name__ == '__main__':
+    run_d()
+    
